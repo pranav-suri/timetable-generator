1c9a86886927cb592013ea9e19699076dbb52023
pranav-suri <pranavsuri4@gmail.com>
2024-04-13 15:00:09 +0530

    Increased Teach entries. [GROUP ASSIGNMENT LEFT]

--- index.py
+++ index.py
@@ -43,20 +43,83 @@ ClassroomTypes = {
 
 for classroom in classrooms:
     if classroom["isLab"]:
-        ClassroomTypes["Lab"].append(classroom['id'])
+        ClassroomTypes["Lab"].append(str(classroom['id']))
     else:
-        ClassroomTypes["notLab"].append(classroom['id'])
+        ClassroomTypes["notLab"].append(str(classroom['id']))
+
+# Below algorithm assummes we are talking about a departmnent
+# There will be more complex in case of multiple departments and electives
+
+# Count number of divisions
+divisionSet = {subdivision["divisionId"] for subdivision in subdivisions}
+numberOfDivisions = len(divisionSet)
+
+# Count number of teacher of a subject (notLab)
+notLabTeachersBySubject = {}
+for teach in teaches:
+    subjectId = teach["subjectId"]
+    # Check if the subject is non-lab
+    if not teach["isLab"]:
+        # Increment the count for the subject
+        if subjectId in notLabTeachersBySubject:
+            notLabTeachersBySubject[subjectId].append(teach["teacherId"])
+        else:
+            notLabTeachersBySubject[subjectId] = [teach["teacherId"]]
+
+# Increase number of teacher entries randomly to equal the number of divisions
+for subjectId, teacherIds in notLabTeachersBySubject.items():
+    if len(teacherIds) < numberOfDivisions:
+        # Randomly select teachers to increase the count
+        numOfSubjectTeachers = len(teacherIds)
+        for i in range(numberOfDivisions - numOfSubjectTeachers):
+            teaches.append({
+                "teacherId": teacherIds[i % numOfSubjectTeachers],
+                "subjectId": subjectId,
+                "isLab": False
+            })
+
+# Count number of subdivisions
+subdivisionSet = {subdivision["subdivisionId"] for subdivision in subdivisions}
+numberOfSubdivisions = len(subdivisionSet)
+
+# Count number of teacher of a subject (Lab)
+labTeachersBySubject = {}
+for teach in teaches:
+    subjectId = teach["subjectId"]
+    # Check if the subject is lab
+    if teach["isLab"]:
+        # Increment the count for the subject
+        if subjectId in labTeachersBySubject:
+            labTeachersBySubject[subjectId].append(teach["teacherId"])
+        else:
+            labTeachersBySubject[subjectId] = [teach["teacherId"]]
+
+# Increase number of teacher entries randomly to equal the number of subdivisions
+for subjectId, teacherIds in labTeachersBySubject.items():
+    if len(teacherIds) < numberOfSubdivisions:
+        # Randomly select teachers to increase the count
+        numOfSubjectTeachers = len(teacherIds)
+        for i in range(numberOfSubdivisions - numOfSubjectTeachers):
+            teaches.append({
+                "teacherId": teacherIds[i % numOfSubjectTeachers],
+                "subjectId": subjectId,
+                "isLab": True
+            })
 
 Classes = []
-
 for teach in teaches:
     Classes.append({
-            "Subject": teach["subjectId"],
-            "Type": "Lab" if teach["isLab"] else "notLab",
-            "Teacher": teach["teacherId"],
+            "Subject": str(teach['subjectId']),
+            "Type": "Lab" if teach["isLab"] else "Theory",
+            "Teacher": str(teach["teacherId"]),
             "ClassroomType": "Lab" if teach["isLab"] else "notLab",
             "Duration": "2" if teach["isLab"] else "1",
+            "Group": []
         })
+# Sort the classes by subjectId
+Classes.sort(key=lambda x: int(x["Subject"]))
+
+# TODO: Add the groups to the classes
 
 
 final = {"ClassroomTypes": ClassroomTypes, "Classes": Classes}
